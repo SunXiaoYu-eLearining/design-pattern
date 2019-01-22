a0485c7300e7e1b3c6338b2c1e517c5b613958f4
sunxiaoyu <sunxiaoyu.space@outlook.com>
2019-01-22 10:05:27 +0800

    completed command pattern

---
+++ src/behavioral/pattern/command/BuyStock.java
@@ -0,0 +1,13 @@
+package behavioral.pattern.command;
+
+public class BuyStock implements Order {
+    private Stock abcStock;
+    public BuyStock(Stock abcStock){
+        this.abcStock = abcStock;
+    }
+
+    @Override
+    public void execute() {
+        abcStock.buy();
+    }
+}
