1199c9e09854d3950caa2dda473944d3bc448ae9
sunxiaoyu <sunxiaoyu.space@outlook.com>
2019-01-30 08:05:43 +0800

    completed strategy, template and visitor pattern

---
+++ src/behavioral/pattern/visitor/Keyboard.java
@@ -0,0 +1,8 @@
+package behavioral.pattern.visitor;
+
+public class Keyboard implements ComputerPart {
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+        computerPartVisitor.visit(this);
+    }
+}
